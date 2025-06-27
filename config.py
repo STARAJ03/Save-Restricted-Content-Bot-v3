a798ea5c35a7e0d9b5ec53cfdf4ceaab06e53f49
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-27 10:34:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for s
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "500000000000"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500000000000"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/+asjRvo7dzMc4YzZl") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/")
 
