c809918a966a54110a54a0455a24a9cf54cd667a
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-30 20:40:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,9 +20,9 @@ YTUB_COOKIES = """
 API_ID = os.getenv("API_ID", "27765349")
 API_HASH = os.getenv("API_HASH", "9df1f705c8047ac0d723b29069a1332b")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://STAR10:<iamagamer>@cluster0.w2snkuc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://allclashwithaj:iamagamer@src.u1x2mpp.mongodb.net/?retryWrites=true&w=majority&appName=SRC")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "1116405290").split())) # list seperated via space
-DB_NAME = os.getenv("DB_NAME", "STAR10")
+DB_NAME = os.getenv("DB_NAME", "SRC")
 STRING = os.getenv("STRING", None) # optional
 LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002409813500")) # optional with -100
 FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002856910838")) # optional with -100
@@ -30,8 +30,8 @@ MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for s
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "500000000000"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500000000000"))
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "3"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "5000"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/+asjRvo7dzMc4YzZl") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/")
 
